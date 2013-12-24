e226e886810aa14c960d601b8e4eaf288799d87b
Alexandre Leray <alexandre@alexandreleray.com>
2013-12-24 12:58:11 +0100

    License

--- aa.jquery.contextual.js
+++ aa.jquery.contextual.js
@@ -1,3 +1,19 @@
+// Copyright 2013 Alexandre Leray <http://stdin.fr/>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
 ;(function ($, window, document, undefined) {
---
+++ img/README.md
@@ -0,0 +1,4 @@
+Icons by the Libre Graphics Reasearch Unit contributors, licensed under the
+Free Art License.
+
+See <http://git.constantvzw.org/?p=lgru.co-position.catalog.git;a=tree>
